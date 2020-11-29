58efb22af259a4945a8534691608d6c7654694bc
ZhaoJianyu <zjyhhube@163.com>
2020-11-29 21:54:14 +0800

    SSM 配置基础搭建

---
+++ src/main/java/com/zjy/service/BookServiceImpl.java
@@ -0,0 +1,37 @@
+package com.zjy.service;
+
+import com.zjy.mapper.BookMapper;
+import com.zjy.pojo.Books;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    //业务层调用dao层
+    @Resource
+    private BookMapper bookMapper;
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBooks() {
+        return bookMapper.queryAllBooks();
+    }
+}
